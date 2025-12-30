a8be33ecb413fc258734818173e304d3b21bb613
LyndaRita-BL <lynda.rita@bridgelabz.com>
2025-12-30 15:01:47 +0530

    [LYNDA] Feat:UC5: Use Map for Character Patterns and Render via Function

---
+++ OOPSBannerUC5.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerUC5 {
+
+    public static void main(String[] args) {
+        Map<Character, String[]> patternMap = buildPatternMap();
+        printBanner("OOPS", patternMap);
+    }
+
+    static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+
+    static void printBanner(String word, Map<Character, String[]> map) {
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                line.append(map.get(ch)[row]).append(" ");
+            }
+            System.out.println(line);
+        }
+    }
+}
