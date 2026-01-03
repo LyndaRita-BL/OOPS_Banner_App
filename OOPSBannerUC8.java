bdd091e03d561ca4c9ae683de1cc5157fa1ce0d1
LyndaRita-BL <lynda.rita@bridgelabz.com>
2026-01-03 13:59:29 +0530

    [Lynda]-Feat:UC8: Use Map for Character Patterns and Render via Function

---
+++ OOPSBannerUC8.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * UC8: Use Map for Character Patterns and Render via Function
+ */
+public class OOPSBannerUC8 {
+
+    public static void main(String[] args) {
+        Map<Character, String[]> patternMap = buildCharacterPatternMap();
+        printBanner("OOPS", patternMap);
+    }
+
+    /**
+     * Builds character-to-pattern map
+     *
+     * @return map of character patterns
+     */
+    static Map<Character, String[]> buildCharacterPatternMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+
+    /**
+     * Prints banner for the given word
+     *
+     * @param word input word
+     * @param map  character pattern map
+     */
+    static void printBanner(String word, Map<Character, String[]> map) {
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                line.append(map.get(ch)[row]).append("   ");
+            }
+            System.out.println(line);
+        }
+    }
+}
