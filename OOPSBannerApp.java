2ef264014db45154114fe637134d1d8e16554f80
LyndaRita-BL <lynda.rita@bridgelabz.com>
2026-01-03 15:32:36 +0530

    [lynda]-feat:Render OOPS as Banner using Print Statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,42 @@
-/**
- * UC1: Print OOPS to Console
- *
- * <p>This program demonstrates a basic Java application
- * that prints the literal text "OOPS" to the console.</p>
- *
- * @author Lynda
- * @version 1.0
- */
-class OOPSBannerApp  {
+// /**
+//  * UC1: Print OOPS to Console
+//  *
+//  * <p>This program demonstrates a basic Java application
+//  * that prints the literal text "OOPS" to the console.</p>
+//  *
+//  * @author Lynda
+//  * @version 1.0
+//  */
+// class OOPSBannerApp  {
 
-    /**
-     * Main method – entry point of the Java application.
-     *
-     * @param args command-line arguments
-     */
+//     /**
+//      * Main method – entry point of the Java application.
+//      *
+//      * @param args command-line arguments
+//      */
+//     public static void main(String[] args) {
+
+//         // Print statement to display output on the console
+//         System.out.println("OOPS");
+//     }
+// }
+/* uc2-Render OOPS as Banner using Print Statements */
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        // Print statement to display output on the console
-        System.out.println("OOPS");
+        String[] banner = {
+                " *****   *****   *****   ***** ",
+                "*     * *     * *     * *      ",
+                "*     * *     * *     * *      ",
+                "*     * *     * *****   ***** ",
+                "*     * *     * *            * ",
+                "*     * *     * *            * ",
+                " *****   *****  *        ***** "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
