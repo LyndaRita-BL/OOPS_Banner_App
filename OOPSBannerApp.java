3f359d2e525be5c97419b0fd60143a0f29a290db
LyndaRita-BL <lynda.rita@bridgelabz.com>
2026-01-03 15:20:12 +0530

    [LYNDA]-Feat:UC1: Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * UC1: Print OOPS to Console
+ *
+ * <p>This program demonstrates a basic Java application
+ * that prints the literal text "OOPS" to the console.</p>
+ *
+ * @author Lynda
+ * @version 1.0
+ */
+class OOPSBannerApp  {
+
+    /**
+     * Main method – entry point of the Java application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print statement to display output on the console
+        System.out.println("OOPS");
+    }
+}
