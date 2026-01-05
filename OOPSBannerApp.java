5de903a5c08462fc9d1a25792ceaee76950594a2
LyndaRita-BL <lynda.rita@bridgelabz.com>
2026-01-05 10:58:51 +0530

    [LYNDA]-Feat: UC4: Render OOPS as Banner using String Array and Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,64 @@
-
-
 public class OOPSBannerApp {
+    /**
+     * Inner Class to store character and its pattern
+     */
+    static class CharacterPattern {
+        private final char character;
+        private final String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
 
     public static void main(String[] args) {
 
-        String[] banner = {
-                " *****   *****   *****   ***** ",
-                "*     * *     * *     * *      ",
-                "*     * *     * *     * *      ",
-                "*     * *     * *****   ***** ",
-                "*     * *     * *            * ",
-                "*     * *     * *            * ",
-                " *****   *****  *        ***** "
-        };
+        CharacterPattern o = new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
 
-        for (String line : banner) {
-            System.out.println(line);
+        CharacterPattern p = new CharacterPattern('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        CharacterPattern s = new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        CharacterPattern[] word = {o, o, p, s};
+
+        for (int i = 0; i < 7; i++) {
+            for (CharacterPattern cp : word) {
+                System.out.print(cp.getPattern()[i] + " ");
+            }
+            System.out.println();
         }
     }
 }
-
