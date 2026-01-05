afd4921ed19ce7663511c878a3840d86d4f1a396
LyndaRita-BL <lynda.rita@bridgelabz.com>
2026-01-05 10:44:12 +0530

    [lynda]-feat:UC3: Render OOPS as Banner using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,5 @@
-// /**
-//  * UC1: Print OOPS to Console
-//  *
-//  * <p>This program demonstrates a basic Java application
-//  * that prints the literal text "OOPS" to the console.</p>
-//  *
-//  * @author Lynda
-//  * @version 1.0
-//  */
-// class OOPSBannerApp  {
 
-//     /**
-//      * Main method – entry point of the Java application.
-//      *
-//      * @param args command-line arguments
-//      */
-//     public static void main(String[] args) {
 
-//         // Print statement to display output on the console
-//         System.out.println("OOPS");
-//     }
-// }
-/* uc2-Render OOPS as Banner using Print Statements */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
@@ -40,3 +19,4 @@ public class OOPSBannerApp {
         }
     }
 }
+
